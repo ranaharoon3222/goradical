70fd55b734d03f48c22b1da5d41a44c07440a26d
ranaharoon3222 <ranaharoon3222@gmail.com>
2021-09-04 16:51:34 +0500

    fixed build

--- src/components/portfolio/modal.js
+++ src/components/portfolio/modal.js
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 
 const Modal = ({ isOpen, closeModal, title, img }) => {
   return (
